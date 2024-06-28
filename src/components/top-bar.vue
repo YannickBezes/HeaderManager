eda84d189b862fb064c44eb29e35e76d82a3c4a7
Yannick <yannick.bezes@gmail.com>
2024-06-28 21:04:02 +0200

    add action buttons

--- src/components/top-bar.vue
+++ src/components/top-bar.vue
@@ -2,6 +2,9 @@
 import ProfileIcon from "./profile-icon.vue";
 import {useProfileStore} from "../stores/profile.store.ts";
 import ProfileName from "./profile-name.vue";
+import AddIcon from "../assets/icons/add.svg";
+import PauseIcon from "../assets/icons/pause.svg";
+import MoreIcon from "../assets/icons/more.svg";
 
 const profileStore = useProfileStore();
 const currentProfile = profileStore.currentProfile;
@@ -9,14 +12,20 @@ const currentProfile = profileStore.currentProfile;
 
 <template>
 	<header class="top-bar" v-if="currentProfile !== null">
-		<profile-icon :profile-id="currentProfile.id" />
-		<profile-name :profile-id="currentProfile.id" />
+		<profile-icon :profile-id="currentProfile.id"/>
+		<profile-name :profile-id="currentProfile.id" :editable="true"/>
+
+		<div class="action-buttons-container">
+			<add-icon class="action-button" tabindex="0"/>
+			<pause-icon class="action-button" tabindex="0"/>
+			<more-icon class="action-button" tabindex="0"/>
+		</div>
 	</header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .top-bar {
-	background-color: var(--grey-140);
+	background-color: var(--grey-180);
 	color: white;
 	position: fixed;
 	width: 100%;
@@ -24,5 +33,32 @@ const currentProfile = profileStore.currentProfile;
 	padding: 10px;
 	display: flex;
 	gap: 10px;
+}
+
+.action-buttons-container {
+	flex: 1 1 auto;
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+	gap: 10px;
+}
+
+.action-button {
+	width: 34px;
+	height: 34px;
+	padding: 5px;
+	cursor: pointer;
+	border-radius: 50%;
+
+	&:hover,
+	&:focus {
+		outline: none;
+		background: var(--grey-110);
+
+		:deep(*) {
+			fill: var(--primary-20);
+
+		}
+	}
 }
 </style>
